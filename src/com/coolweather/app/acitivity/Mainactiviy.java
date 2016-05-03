b0861a64da21449c0c333b645935265a6c753450
unknown <Administrator@huanghongqi-pc.ATA.LOCAL>
2016-05-03 15:30:03 +0800

    增加包名

--- src/com/coolweather/app/acitivity/Mainactiviy.java
+++ src/com/coolweather/app/acitivity/Mainactiviy.java
@@ -1,5 +1,18 @@
 package com.coolweather.app.acitivity;
 
-public class Mainactiviy {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Mainactiviy extends Activity {
+
+
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		
+		
+		
+	}
 
 }
--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -8,15 +8,39 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
+	/**
+	 * Province表建表语句
+	 */
+
+	private static final String CREATE_PROVINCE = "create table province (id integer primary key autoincrement,province_name text,province_code text )";
+
+	/**
+	 * City表建表语句
+	 */
+	public static final String CREATE_CITY = "create table City (" + "id integer primary key autoincrement, "
+			+ "city_name text, " + "city_code text, " + "province_id integer)";
+
+	/**
+	 * County表建表语句
+	 */
+	public static final String CREATE_COUNTY = "create table County (" + "id integer primary key autoincrement, "
+			+ "county_name text, " + "county_code text, " + "city_id integer)";
+
 	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
 		super(context, name, factory, version);
 
+		
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
+		db.execSQL(CREATE_PROVINCE); //创建Province 表
+		db.execSQL(CREATE_CITY); //创建city表
+		db.execSQL(CREATE_COUNTY);//创建county表
+		
 
+		
+		
 	}
 
 	@Override
